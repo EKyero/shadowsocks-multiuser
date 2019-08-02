57262eef49d2cf6564735cb4aaffe12a9f62abac
Connection Refused <ConnectionRefusedX@protonmail.com>
2019-08-02 15:38:51 +0800

    Add last use time

--- bandwidth.go
+++ bandwidth.go
@@ -1,12 +1,16 @@
 package main
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 // Bandwidth struct
 type Bandwidth struct {
 	sync.Mutex
 	Upload   uint64
 	Download uint64
+	Last     int64
 }
 
 // IncreaseUpload for increase uploaded size
@@ -14,6 +18,7 @@ func (bandwidth *Bandwidth) IncreaseUpload(size uint64) {
 	bandwidth.Lock()
 	defer bandwidth.Unlock()
 	bandwidth.Upload += size
+	bandwidth.Last = time.Now().Unix()
 }
 
 // IncreaseDownload for increase downloaded size
@@ -21,6 +26,7 @@ func (bandwidth *Bandwidth) IncreaseDownload(size uint64) {
 	bandwidth.Lock()
 	defer bandwidth.Unlock()
 	bandwidth.Download += size
+	bandwidth.Last = time.Now().Unix()
 }
 
 // Reset for reset uploaded and downloaded size
@@ -29,10 +35,12 @@ func (bandwidth *Bandwidth) Reset() {
 	defer bandwidth.Unlock()
 	bandwidth.Upload = 0
 	bandwidth.Download = 0
+	bandwidth.Last = time.Now().Unix()
 }
 
 func newBandwidth() *Bandwidth {
 	bandwidth := Bandwidth{}
+	bandwidth.Last = 0
 
 	return &bandwidth
 }
