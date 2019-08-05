031e64faf669cd50df5a722204f8b5cf8e59156f
Connection Refused <ConnectionRefusedX@protonmail.com>
2019-08-05 20:41:39 +0800

    Replace

--- core/cipher.go
+++ core/cipher.go
@@ -7,8 +7,8 @@ import (
 	"sort"
 	"strings"
 
-	"github.com/shadowsocks/go-shadowsocks2/shadowaead"
-	"github.com/shadowsocks/go-shadowsocks2/shadowstream"
+	"github.com/NetchX/go-shadowsocks2/shadowaead"
+	"github.com/NetchX/go-shadowsocks2/shadowstream"
 )
 
 type Cipher interface {
